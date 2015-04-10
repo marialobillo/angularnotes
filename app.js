c0a35d61ed81e25576bbf8f4a93b567a29ca07c5
Neniah <maria.lobillo.santos@gmail.com>
2015-04-10 12:05:04 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var work = function(){
+
+	console.log("working hard!");
+};
+
+var doWork = function(f){
+
+	console.log("starting");
+
+	try{
+		f();
+	}catch(ex){
+		console.log(ex);
+	}
+	console.log("end");
+
+};
+
+doWork(work);
