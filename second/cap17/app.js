fc11ab7a292efc9757b1ce0e64c676cee7f6be64
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-09-18 19:22:54 +0200

    cap 18

--- second/cap17/app.js
+++ second/cap17/app.js
@@ -1,7 +1,13 @@
 
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', ['$scope', function($scope){
+myApp.controller('mainController', ['$scope', '$timeout', function($scope, $timeout){
 
   $scope.name = 'Maria';
+
+  $timeout(function(){
+
+    $scope.name = 'Everybody';
+    
+  }, 3000);
 }]);
